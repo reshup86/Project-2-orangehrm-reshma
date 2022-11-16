e3264af6b331837e5d00ff8ee0f3434ed26dd5cd
reshup86 <reshup86@gmail.com>
2022-11-16 14:05:22 +0000

    Project-2-orangehrm-selenium

---
+++ src/orange_hrm/Edge_Browser.java
@@ -0,0 +1,36 @@
+package orange_hrm;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class Edge_Browser {
+    public static void main(String[] args) {
+        String baseURL = "https://opensource-demo.orangehrmlive.com/";
+        System.setProperty("webdriver.edge.driver", "drivers/msedgedriver.exe");
+
+        WebDriver driver = new EdgeDriver();
+        driver.get(baseURL);
+
+        //get the title of the page
+        String title = driver.getTitle();
+
+        //Print the current URL
+        System.out.println("Current URL is: " +driver.getCurrentUrl());
+
+        //Print the page source
+        System.out.println("Page source is: " +driver.getPageSource());
+
+        //Enter the email to email field
+        WebElement usernameField = driver.findElement(By.name("username"));
+        usernameField.sendKeys("Admin");
+
+        //Enter the password field
+        WebElement passwordField = driver.findElement(By.name("password"));
+        passwordField.sendKeys("admin123");
+
+        //Close the browser
+        driver.quit();
+    }
+}
---
+++ src/orange_hrm/FireFox_Browser.java
@@ -0,0 +1,42 @@
+package orange_hrm;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class FireFox_Browser {
+    public static void main(String[] args) {
+        String baseURL = "https://opensource-demo.orangehrmlive.com/";
+        System.setProperty("webdriver.firefox.driver", "drivers/geckodriver.exe");
+
+        WebDriver driver = new FirefoxDriver();
+        driver.get(baseURL);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+        //get the title of the page
+        String title = driver.getTitle();
+
+        //Print the current URL
+        System.out.println("Current URL is: " +driver.getCurrentUrl());
+
+        //Print the page source
+        System.out.println("Page source is: " +driver.getPageSource());
+
+        //Enter the email to email field
+
+        WebElement usernameField = driver.findElement(By.name("username"));
+        usernameField.sendKeys("Admin");
+
+        //Enter the password field
+
+        WebElement passwordField = driver.findElement(By.name("password"));
+        passwordField.sendKeys("admin123");
+
+        //Close the browser
+        driver.quit();
+    }
+}
